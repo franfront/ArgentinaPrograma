5c20538c77c84e68db5bdea15e746956d47ef820
Franco <fernandezfrancoa2001@gmail.com>
2022-05-29 02:37:22 -0300

    componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAppComponent } from './components/logo-app/logo-app.component';
-import { SocialComponent } from './components/social/social.component';
 import { AboutComponent } from './components/about/about.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAppComponent,
-    SocialComponent,
     AboutComponent,
     BannerComponent,
     ExperienceComponent,
     EducationComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
